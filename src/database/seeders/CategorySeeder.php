07923209df109daafbdfe74abf1f1b914bd7d7cd
Daniil <daniliyo.p1@gmail.com>
2023-03-28 18:58:54 +0300

    Add Category model, migration and seeder.

---
+++ src/database/seeders/CategorySeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $categoryArr = [];
+        for($i = 1; $i < 11; $i++){
+            $categoryArr[] = [
+                'name' => 'Category '.$i,
+            ];
+        }
+        DB::table('categories')->insert($categoryArr);
+    }
+}
